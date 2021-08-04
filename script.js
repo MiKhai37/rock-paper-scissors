1e69eb18c67d79535fcfbd9b7827d116850b77f3
MiKhai37 <mi.khai.thai@gmail.com>
2021-08-04 16:36:34 +0200

    Add style.css, and edit files

--- script.js
+++ script.js
@@ -8,21 +8,19 @@ function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
   }
 
-console.log(computerPlay())
-
 function playRPS(playerSelection, computerSelection) {
-    const pSelect = playerSelection.toLowerCase();
-    const cSelect = computerSelection.toLowerCase();
-    const tupSelect = (pSelect, cSelect);
     const rpsState= {
-        1: ("rock", "paper"),
-        2: ("scissors", "rock"),
-        3: ("paper", "scissors"),
-        4: ("paper", "rock"),
-        5: ("scissors", "paper"),
-        6: ("rock", "scissors"),
+        1: ["rock", "paper"],
+        2: ["scissors", "rock"],
+        3: ["paper", "scissors"],
+        4: ["paper", "rock"],
+        5: ["scissors", "paper"],
+        6: ["rock", "scissors"],
     }
-    const key = getKeyByValue(rpsState, (playerSelection, computerSelection));
+    const key = getKeyByValue(rpsState, [playerSelection.toLowerCase(), computerSelection.toLowerCase()]);
+
+    console.log(key)
+
     if (key>3) {
         return "Congratulations, you win";
     } else if (key<=3) {
@@ -31,3 +29,18 @@ function playRPS(playerSelection, computerSelection) {
         return "Tie";
     }
 }
+
+function game()  {
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 0; I < 5; I++) {
+
+    }
+}
+
+const a = "scissors"
+const b = computerPlay().toLowerCase()
+
+console.log(a)
+console.log(b)
+console.log(playRPS(a, b))
