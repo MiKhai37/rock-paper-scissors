3e39a0aa05e06ab65f764d257950e2203408c6a7
MiKhai37 <mi.khai.thai@gmail.com>
2021-08-05 11:47:15 +0200

    Edit script.js (full logic)

--- script.js
+++ script.js
@@ -1,38 +1,88 @@
+// Random Select between rock, paper and scissors
 function computerPlay() {
-    const rpsChoice = ["Rock", "Parper", "Scissors"];
+    const rpsChoice = ["Rock", "Paper", "Scissors"];
     const randInt = Math.floor(Math.random() *  3);
     return rpsChoice[randInt];
 };
 
-function playRPS(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) return "Tie";
-    switch (playerSelection.toLowerCase()) {
+// Play logic of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+
+    playerLower = playerSelection.toLowerCase()
+    computerLower = computerSelection.toLowerCase()
+
+    if (playerLower === computerLower) {
+        console.log("Tie!")
+        return 0;
+    }
+
+    switch (playerLower) {
 
         case "rock" :
-            if (computerSelection.toLowerCase() == "paper") return "Loser rock vs paper"
-            else return "Winner rock vs scissors"
+            if (computerLower == "paper") {
+                console.log("Loser rock vs paper")
+                return -1
+            } else {
+                console.log("Winner rock vs scissors")
+                return 1
+            }
 
         case "paper" :
-            if (computerSelection.toLowerCase() == "rock") return "Loser paper vs rock"
-            else return "Winner paper vs scissors"
+            if (computerLower == "rock") {
+                console.log("Loser paper vs rock")
+                return -1
+            } else {
+                console.log("Winner paper vs scissors")
+                return 1
+            }
 
         case "scissors":
-            if (computerSelection.toLowerCase() == "rock") return "Loser scissors vs rock"
-            else return "Winner scissors vs paper"
+            if (computerLower == "rock") {
+                console.log("Loser scissors vs rock")
+                return -1
+            } else {
+                console.log("Winner scissors vs paper")
+                return 1
+            }
+    }
+}
+// Play n times, and show scores
+function game(n=5)  {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < n; i++) {
+        console.log("Round: " + (i+1));
+
+        playerSelection = prompt();
+        computerSelection = computerPlay();
+
+        score = playRound(playerSelection, computerSelection);
+
+        if (score == 1) playerScore += 1;
+        if (score == -1) computerScore += 1;
+
+        console.log("Player Score: " + playerScore);
+        console.log("Computer Score: " + computerScore);
+    }
+
+    if (playerScore == computerScore) {
+        console.log("Tie! Nobody win!");
+    } else if (playerScore > computerScore) {
+        console.log("Player wins!");
+    } else {
+        console.log("Computer wins!");
     }
 }
 
-function game()  {
-    playerScore = 0;
-    computerScore = 0;
-    for (let i = 0; I < 5; I++) {
-        playRPS('rock', computerPlay())
-    }
-}
+// Test the logic implemented
+/*const playerSelection = 'Rock'
+const computerSelection = computerPlay()
 
-const a = "scissors"
-const b = computerPlay().toLowerCase()
+console.log("player plays: " + playerSelection)
+console.log("computer plays: " + computerSelection)
 
-console.log(a)
-console.log(b)
-console.log(playRPS(a, b))
+console.log(playRound(playerSelection, computerSelection))*/
+
+game()
+
+
